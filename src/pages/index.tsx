062fd10ef14e74164243e10786071f9ee1a2f004
thiagnmiziara <thiagonmiziara@gmail.com>
2024-08-04 09:11:34 -0300

    feat: layout home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,27 @@
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+import camiseta1 from "@/assets/camisetas/camiseta1.png";
+import camiseta2 from "@/assets/camisetas/camiseta2.png";
+import camiseta3 from "@/assets/camisetas/camiseta3.png";
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  return (
+    <HomeContainer>
+      <Product>
+        <Image src={camiseta1} alt='camiseta1' width={520} height={480} />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+
+      <Product>
+        <Image src={camiseta2} alt='camiseta1' width={520} height={480} />
+        <footer>
+          <strong>Camiseta x</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+    </HomeContainer>
+  );
 }
