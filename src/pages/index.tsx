de6bd6af4fe28324b937c72dc29eb29c5e16a1be
thiagnmiziara <thiagonmiziara@gmail.com>
2024-08-04 11:07:42 -0300

    fix: adjust format price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
+import Image from "next/image";
+import Link from "next/link";
 import Stripe from "stripe";
 import { useKeenSlider } from "keen-slider/react";
 import { HomeContainer, Product } from "@/styles/pages/home";
-import Image from "next/image";
 import { stripe } from "@/lib/stripe";
-import camiseta1 from "@/assets/camisetas/camiseta1.png";
 
 import "keen-slider/keen-slider.min.css";
 
@@ -30,19 +30,21 @@ export default function Home({ products }: THomeProps) {
   return (
     <HomeContainer ref={sliderRef} className='keen-slider'>
       {products.map((product) => (
-        <Product className='keen-slider__slide' key={product.id}>
-          <Image src={product.imageUrl} alt='' width={520} height={480} />
-          <footer>
-            <strong>{product.name}</strong>
-            <span>R$ {product.price}</span>
-          </footer>
-        </Product>
+        <Link href={`/product/${product.id}`} key={product.id}>
+          <Product className='keen-slider__slide'>
+            <Image src={product.imageUrl} alt='' width={520} height={480} />
+            <footer>
+              <strong>{product.name}</strong>
+              <span>{product.price}</span>
+            </footer>
+          </Product>
+        </Link>
       ))}
     </HomeContainer>
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const response = await stripe.products.list({
     expand: ["data.default_price"],
   });
@@ -53,7 +55,10 @@ export const getServerSideProps: GetServerSideProps = async () => {
       id: product.id,
       name: product.name,
       imageUrl: product.images[0],
-      price: price.unit_amount !== null ? price.unit_amount / 100 : 0,
+      price: new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(price.unit_amount) / 100),
     };
   });
 
@@ -61,5 +66,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       products,
     },
+    revalidate: 60 * 60 * 24,
   };
 };
