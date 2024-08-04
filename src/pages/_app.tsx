dde84a493774a652cacb6aedb595da1bd2272166
thiagnmiziara <thiagonmiziara@gmail.com>
2024-08-04 07:30:41 -0300

    feat: create header and styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
-import { globalStyles } from "@/styles/global";
 import type { AppProps } from "next/app";
+import Image from "next/image";
+
+import logoImg from "@/assets/logo.svg";
+
+import { globalStyles } from "@/styles/global";
+import { Container, Header } from "@/styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <Image
+          src={logoImg}
+          alt='logo com dois triângulos verdes um sobre o outro e com o titulo ignite shop'
+        />
+      </Header>
+      <Component {...pageProps} />;
+    </Container>
+  );
 }
