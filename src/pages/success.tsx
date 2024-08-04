38113dd08996850d3f73bfe2095ec0592eb2c968
thiagnmiziara <thiagonmiziara@gmail.com>
2024-08-04 14:04:42 -0300

    feat: create styles and structure page  success

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -1,3 +1,20 @@
+import { SuccessContainer, ImageContainer } from "@/styles/pages/success"
+import Link from "next/link"
+
 export default function Success() {
-  return <h1>Sucesso!</h1>
+  return (
+    <SuccessContainer>
+      <h1>Compra efetuada!</h1>
+
+      <ImageContainer></ImageContainer>
+
+      <p>
+        Uhuul <strong>Thiago Miziara</strong>, sua{" "}
+        <strong>Camiseta Beyond the Limits</strong>
+        já está a caminho da sua casa.
+      </p>
+
+      <Link href="/">Voltar ao catálogo</Link>
+    </SuccessContainer>
+  )
 }
